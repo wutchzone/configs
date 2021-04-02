f7d8eeec5f0b0611b1fc72a870b98becb1bf06a6
Daniel Sedlak <daniel@sedlak.biz>
2021-04-02 20:44:43 +0200

    better i3status

---
+++ i3/i3status.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+i3status | while :
+do
+    read line
+    id=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
+    name=$(xprop -id $id | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
+    node -e 'var carry=process.argv[1];try{var obj=JSON.parse(carry.slice(1,carry.length));if(obj instanceof Array){obj=[{name:"app_name",markup:"none",full_text:process.argv[2]}].concat(obj)};console.log((obj instanceof Array ? ",":"")+JSON.stringify(obj))}catch{console.log(carry)}' -- "$line" "$name"
+done
+
