a040b16984f3de15c7751418a28b3d827515d116
Daniel Sedlak <daniel@sedlak.biz>
2022-02-09 21:43:53 +0100

    Update screenlayout

--- screenlayout/moria.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DVI-I-0 --off --output DVI-I-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-0 --off --output DP-1 --off --output DVI-D-0 --mode 1920x1080 --pos 3840x0 --rotate normal --output DVI-I-1-0 --off --output DVI-I-1-1 --off --output HDMI-1-0 --off --output DP-1-0 --off --output DP-1-1 --off --output DVI-D-1-0 --off
